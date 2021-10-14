9ff86b7cdd130ff705eaa265948de671791b3bd5
fumengmeng <757854758@qq.com>
2021-10-14 21:42:59 +0800

    1

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -3,9 +3,9 @@
 
 pragma solidity ^0.8.0;
 
-import "./openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./iNft.sol";
-import "./openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Nft is Ownable, ERC721, INFT {
     
